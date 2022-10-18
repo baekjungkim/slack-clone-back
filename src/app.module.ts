1cbfa73a5f41cc1cd7d1964096eafe69f8ff6a6d
Kim Baekjung <zvgandam@gmail.com>
2022-10-18 17:57:25 +0900

    feat: ConfigModule 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 외부에서 .env 키 조회 가능
+// const getEnv = async () => {
+//   const response = await axios.get('/비밀키 요청');
+//   return response.data
+// return { DATABSE_NAME: '' };
+// };
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // load: [getEnv]
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
