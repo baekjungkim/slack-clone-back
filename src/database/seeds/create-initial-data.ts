de12f53330fa8f1c03fb8a70887fb9b8433bce31
Kim Baekjung <zvgandam@gmail.com>
2022-10-20 17:21:35 +0900

    feat: seed data 생성

---
+++ src/database/seeds/create-initial-data.ts
@@ -0,0 +1,13 @@
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+import { Workspaces } from '../../entities/Workspaces';
+import { Channels } from '../../entities/Channels';
+
+export default class UserSeeder implements Seeder {
+  public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
+    const wrokspacesRepository = dataSource.getRepository(Workspaces);
+    await wrokspacesRepository.insert({ id: 1, name: 'Sleact', url: 'sleact' });
+    const channelsRepository = dataSource.getRepository(Channels);
+    await channelsRepository.insert({ id: 1, name: '일반', WorkspaceId: 1, private: false });
+  }
+}
