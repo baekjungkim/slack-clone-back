54dbd46920950bfe1427ab2120239c265b877b9a
Kim Baekjung <zvgandam@gmail.com>
2022-10-20 17:19:43 +0900

    feat: 엔티티 추가

---
+++ src/entities/Channels.ts
@@ -0,0 +1,60 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ChannelChats } from './ChannelChats';
+import { ChannelMembers } from './ChannelMembers';
+import { Users } from './Users';
+import { Workspaces } from './Workspaces';
+
+@Index('channels_WorkspaceId', ['WorkspaceId'], {})
+@Entity({ name: 'channels' })
+export class Channels {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'name', length: 30 })
+  name: string;
+
+  @Column('boolean', {
+    name: 'private',
+    nullable: true,
+    default: () => "'0'",
+  })
+  private: boolean | null;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column('int', { name: 'WorkspaceId', nullable: true })
+  WorkspaceId: number | null;
+
+  @OneToMany(() => ChannelChats, (channelchats) => channelchats.Channel)
+  ChannelChats: ChannelChats[];
+
+  @OneToMany(() => ChannelMembers, (channelMembers) => channelMembers.Channel, {
+    cascade: ['insert'],
+  })
+  ChannelMembers: ChannelMembers[];
+
+  @ManyToMany(() => Users, (users) => users.Channels)
+  Members: Users[];
+
+  @ManyToOne(() => Workspaces, (workspaces) => workspaces.Channels, {
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'WorkspaceId', referencedColumnName: 'id' }])
+  Workspace: Workspaces;
+}
